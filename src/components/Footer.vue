e888de51148f7aece14b607fe6b76f9f39468723
Amr Saher <amrosaher1234@gmail.com>
2023-03-28 05:24:35 -0700

    Style Home View

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="
+      w-full z-50 flex
+      justify-around p-1
+      mb-2 border-t-2
+      border-t-gray-200
+    ">
+      <div class="grid place-items-center">
+        <HomeIcon :size="30" fillColor="#1f1e1e" />
+        <div class="w-full mt-1 text-xs text-center text-gray-800 font-semibold">
+          Home
+        </div>
+      </div>
+      <div class="grid place-items-center">
+        <MapMarkerIcon :size="30" fillColor="#b3b1b1" />
+        <div class="w-full mt-1 text-xs text-center text-gray-400 font-semibold">
+          Destination
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script setup>
+import HomeIcon from 'vue-material-design-icons/Home.vue'
+import MapMarkerIcon from 'vue-material-design-icons/MapMarker.vue'
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="Home" class="flex flex-col h-screen">
+    <div class="m-3 mb-auto">
+      <div class="my-20">
+        <img width="150" src="img/uber/uber2.svg" alt="logo">
+      </div>
+
+      <div class="grid grid-cols-2 gap-3 my-3">
+        <ServiceSelectLarge 
+          text="Ride"
+          image="ride"
+          imageWidth="74"
+        />
+        <ServiceSelectLarge 
+          text="Food"
+          image="food"
+          imageWidth="74"
+          :promo="true"
+        />
+      </div>
+      <div class="grid grid-cols-4 gap-3 my-3">
+        <ServiceSelectSmall
+          text="2 Wheels"
+          image="2wheels"
+        />
+        <ServiceSelectSmall
+          text="Transit"
+          image="transit"
+        />
+        <ServiceSelectSmall
+          text="Reserve"
+          image="reserve"
+        />
+        <ServiceSelectSmall
+          text="Travel"
+          image="travel"
+        />
+      </div>
+
+      <div class="
+        w-full bg-custom-color
+        h-14 rounded-full flex
+        items-center p-3
+      ">
+        <MagnifyIcon :size="40" />
+        <div class="ml-3 text-xs font-semibold text-gray-700">
+          Enter pick-up point
+        </div>
+      </div>
+    </div>
+
+    <Footer />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import ServiceSelectLarge from '@/components/ServiceSelectLarge.vue'
+import ServiceSelectSmall from '@/components/ServiceSelectSmall.vue'
+import Footer from '@/components/Footer.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+import MagnifyIcon from 'vue-material-design-icons/Magnify.vue'
+</script>
+
+<style lang="scss" scoped>
+#Home {
+  .bg-custom-color {
+    background-color: rgb(237, 237, 237);
   }
 }
-</script>
+</style>
